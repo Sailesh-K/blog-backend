65560a9d05f73334fd866c411596814dd82edebf
Sailesh-K <saileshkv29@gmail.com>
2024-08-17 16:23:31 +0530

    cors update

--- server.js
+++ server.js
@@ -6,7 +6,20 @@ const cors=require("cors");
 const cookieParser = require("cookie-parser");
 
 const app=express();
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+const allowedOrigins = ['http://localhost:5173', 'https://blog-front-end-11.netlify.app'];
+
+app.use(cors({
+  origin: function(origin, callback){
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = 'The CORS policy for this site does not allow access from the specified origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true // Allow credentials (cookies, authorization headers, etc.)
+}));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use('/uploads',express.static(__dirname+'/uploads'));
