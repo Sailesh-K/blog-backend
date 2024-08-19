732a840dc057045cb0c9c56b3e72181192dc867c
Sailesh-K <saileshkv29@gmail.com>
2024-08-19 19:57:00 +0530

    update

--- server.js
+++ server.js
@@ -6,21 +6,9 @@ const cors=require("cors");
 const cookieParser = require("cookie-parser");
 
 const app=express();
-const allowedOrigins = ['http://localhost:5173', 'https://blog-front-end-11.netlify.app'];
 
-app.use(cors({
-  origin: function(origin, callback){
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      const msg = 'The CORS policy for this site does not allow access from the specified origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  methods:['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  credentials: true // Allow credentials (cookies, authorization headers, etc.)
-}));
+
+app.use(cors('*'));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use('/uploads',express.static(__dirname+'/uploads'));
@@ -35,7 +23,6 @@ mongoose
     console.log("Error connecting to DB",err);
 });
 
-//console.log("process.env.DATABASE",process.env.DATABASE);
 
 app.use("/api",routes);
 
