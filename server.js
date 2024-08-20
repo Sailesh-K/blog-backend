4c2b377d36353066889d9f1002bedafb64208c83
Sailesh-K <saileshkv29@gmail.com>
2024-08-20 20:09:49 +0530

    cors methods

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const allowedOrigins = ["http://localhost:5173"];
 app.use(cors({
   origin: allowedOrigins,
   credentials: true, 
+  methods:["GET","POST","PUT","DELETE"]
 }));
 app.use(bodyParser.json());
 app.use(cookieParser());
