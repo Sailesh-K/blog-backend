d9d99864e319340f8fd871e2f221e865fee1f397
Sailesh-K <saileshkv29@gmail.com>
2024-08-20 20:54:36 +0530

    cors again

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app=express();
 
 const allowedOrigins = ["http://localhost:5173"];
 app.use(cors({
-  origin: allowedOrigins,
+  origin: '*',
   credentials: true, 
   methods:["GET","POST","PUT","DELETE"]
 }));
